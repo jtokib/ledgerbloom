63ec10cf913b3567798bd681927c53aee7122851
Toki Burke <jtokib@gmail.com>
2025-08-29 04:54:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,20 +3,9 @@
 
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import { FirebaseAppProvider } from 'reactfire';
-import { AuthProvider, getAuth } from 'firebase/auth';
+import { FirebaseAppProvider, AuthProvider } from 'reactfire';
+import { getAuth } from 'firebase/auth';
 import { app } from '@/lib/firebase';
-
-const firebaseConfig = {
-  projectId: "ledgerbloom",
-  appId: "1:826516699519:web:123983de7b8f2d5a5c824a",
-  storageBucket: "ledgerbloom.firebasestorage.app",
-  apiKey: "AIzaSyDcgty6AvlLVEiWYqxg59IQ8Mv2r9sz5Ok",
-  authDomain: "ledgerbloom.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "826516699519"
-};
-
 
 export default function RootLayout({
   children,
@@ -31,7 +20,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <FirebaseAppProvider firebaseConfig={firebaseConfig} firebaseApp={app}>
+        <FirebaseAppProvider firebaseApp={app}>
           <AuthProvider sdk={getAuth(app)}>
             {children}
             <Toaster />
